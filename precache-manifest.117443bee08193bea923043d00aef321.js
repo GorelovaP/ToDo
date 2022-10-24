9a2c6da863864dd6f7197e98375e981c8357066b
GorelovaP <komsomolskai1@mail.ru>
2022-10-24 12:39:00 +0300

    Updates

---
+++ precache-manifest.117443bee08193bea923043d00aef321.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb6bdc218fb454a38e33049e083019fe",
+    "url": "/ToDo/index.html"
+  },
+  {
+    "revision": "f27114c2d020794125db",
+    "url": "/ToDo/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "b853e9c1599992e97f8f",
+    "url": "/ToDo/static/js/2.1f608c14.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/ToDo/static/js/2.1f608c14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f27114c2d020794125db",
+    "url": "/ToDo/static/js/main.3070aa17.chunk.js"
+  },
+  {
+    "revision": "17bdeea83c1f59c3c01e",
+    "url": "/ToDo/static/js/runtime-main.3fde01ae.js"
+  }
+]);
